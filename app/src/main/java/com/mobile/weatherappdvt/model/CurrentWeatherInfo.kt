cedd1e3bf22c28d0e677deee9cfedd697ca25b46
Yusuf <yusuf.isaacs@rapiddeploy.com>
2022-01-08 12:22:03 +0200

    Create current weather models

---
+++ app/src/main/java/com/mobile/weatherappdvt/model/CurrentWeatherInfo.kt
@@ -0,0 +1,25 @@
+package com.mobile.weatherappdvt.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CurrentWeatherInfo(
+    @SerializedName("base")
+    val base: String,
+    @SerializedName("cod")
+    val cod: Int,
+    @SerializedName("dt")
+    val dt: Int,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("main")
+    val main: Main,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("timezone")
+    val timezone: Int,
+    @SerializedName("visibility")
+    val visibility: Int,
+    @SerializedName("weather")
+    val weather: List<Weather>
+)
