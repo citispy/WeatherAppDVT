6bdecd0f1619414c37ad6267bbd861aaa9f28bfc
Yusuf <yusuf.isaacs@rapiddeploy.com>
2022-01-08 12:47:28 +0200

    Use new packages

--- app/src/main/java/com/mobile/weatherappdvt/ui/MainActivity.kt
+++ app/src/main/java/com/mobile/weatherappdvt/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mobile.weatherappdvt
+package com.mobile.weatherappdvt.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobile.weatherappdvt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
